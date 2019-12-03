c78cd584d20eaa58ce72bd47f156ad6f16df5535
Mattia46 <mattia.assogna@gmail.com>
2019-12-03 17:41:58 +0000

    initSession

--- scripts/initSession.sh
+++ scripts/initSession.sh
@@ -3,43 +3,34 @@
 source ./colors.sh
 
 echo 'Starting the new session'
-tmux kill-session -a
-cd ~/.vim/scripts; ./updateNotes.sh
-tmux new-session -d 'Mattia'
-#tmux split-window -v 'ipython'
-#tmux split-window -h
-#tmux new-window
-#tmux rename-window 'cc'
-#tmux new-window
-#tmux rename-window 'Mattiac'
+tmux kill-server
+#cd ~/.vim/scripts; ./updateNotes.sh
+tmux new-session -d
 
 createWindowTarget() {
-    echo `creating a window $1`
     tmux new-window
     tmux rename-window $1
-    echo `DAZN/$1-bootstrap-dazn`
+    cd ~/DAZN/$1-bootstrap-dazn
 }
 
 createWindowModule() {
-    echo `creating a window $1`
     tmux new-window
     tmux rename-window $1
-    #cd ~/DAZN/$1
+    cd ~/DAZN/$1
 }
 
 startServer() {
-    echo `starting server`
     tmux new-window
     tmux rename-window $1
     #cd ~/DAZN/common-web-bootstrap-module
 }
+
 
 createWindowTarget 'common-web'
 createWindowTarget 'common-tv'
 createWindowModule 'bootstrap-modules'
 startServer 'server'
 tmux -2 attach-session -d
-
 echo 'Completed'
 
 
